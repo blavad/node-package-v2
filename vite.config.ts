7e85e6c6673a09bbeb4c9291702c54a40f34e13f
blavad <david.albert@cegetel.net>
2024-02-03 23:17:38 +0100

    add .d.ts files

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,4 +13,5 @@ export default defineConfig({
 		},
 	},
 	resolve: { alias: { src: resolve("src/") } },
+	plugins: [dts()],
 });
